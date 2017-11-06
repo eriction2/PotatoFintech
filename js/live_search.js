3f47648458f1d2844c54f6b165f28e47882d9c76
eriction2 <eriction2@users.noreply.github.com>
2017-11-06 17:42:37 +0800

    general layout improved + first changes for SplitIt

---
+++ js/live_search.js
@@ -0,0 +1,12 @@
+/*!
+ * Start Bootstrap - New Age v4.0.0-beta.2 (https://startbootstrap.com/template-overviews/new-age)
+ * Copyright 2013-2017 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-new-age/blob/master/LICENSE)
+ */
+
+
+$('#example-one').hierarchySelect({
+    width: 200
+});
+
+
